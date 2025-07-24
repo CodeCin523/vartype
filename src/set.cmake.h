6416e86075b03579468143cf61b0655e0377b199
KurabeTV <155215757+KurabeTV@users.noreply.github.com>
2025-07-23 21:37:22 -0400

    string copy setting added

--- src/set.cmake.h
+++ src/set.cmake.h
@@ -7,6 +7,7 @@
 
 // compare algorithm to use
 #define SET_STRCMP_CHAR     ${VRT_SET_STRCMP_CHAR}
+#define SET_CPYNAME         ${VRT_SET_CPYNAME}
 #define SET_STRCMP_ADDR     ${VRT_SET_STRCMP_ADDR}
 
 // lookup/storage algorithm to use
@@ -17,6 +18,5 @@
 #define SET_THREAD_SAFETY   ${VRT_SET_THREAD_SAFETY}
 // authorization to use calloc(), malloc(), and free() for a specific data type
 #define SET_CALLOC_USAGE    ${VRT_SET_CALLOC_USAGE}
-
 
 #endif
