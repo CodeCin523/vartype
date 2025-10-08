665041720d218b7cc5cebf8b015f5bbfa99c5e1c
CodeCin523 <155215757+CodeCin523@users.noreply.github.com>
2025-10-07 22:30:14 -0400

    test for vrt_size corrections.

--- test/vrt_size.c
+++ test/vrt_size.c
@@ -1,3 +1,4 @@
+#include "vartype/vrt_result.h"
 #include <vartype/vrt_size.h>
 
 #include <stdio.h>
@@ -7,18 +8,20 @@ void main() {
     VRTsize size = 0;
 
     // CHECK FOR ERRORS
-    printf("VRT_ByteToSize Check for the pointer? %d\n", VRT_ByteToSize(1, &size) == VRT_RESULT_SUCCESS);
-    printf("VRT_SizeToByte Check for the pointer? %d\n", VRT_SizeToByte(1, &byte) == VRT_RESULT_SUCCESS);
+    printf("VRT_ByteToSize Check for the pointer? %d\n", VRT_ByteToSize(1, NULL) != VRT_RESULT_SUCCESS);
+    printf("VRT_SizeToByte Check for the pointer? %d\n", VRT_SizeToByte(1, NULL) != VRT_RESULT_SUCCESS);
 
+    printf("VRT_ByteToSize Check for 0 bytes? %d\n", VRT_ByteToSize(0, &size) != VRT_RESULT_SUCCESS);
     printf("VRT_SizeToByte Check for lock? %d\n", VRT_SizeToByte(0b10000000, &byte) != VRT_RESULT_SUCCESS);
 
     // CHECK FOR CORRECT
-    for(byte = 0; byte <= 16; ++byte) {
+    // start at one, since a size of 0 byte is invalid
+    for(byte = 1; byte <= 16; ++byte) {
         size = 0;
         if(VRT_ByteToSize(byte, &size) != VRT_RESULT_SUCCESS)
             printf("Unexpected error !!\n");
-
-        printf("The VRTsize of %d is %d.\n", byte, size);
+        else
+            printf("The VRTsize of %d is %d.\n", byte, size);
     }
 
     for(size = 0; size <= 16; ++size) {
