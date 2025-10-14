5d8ee1dea9ac3bf66e59757d0d6c38e2d708f17d
kurabeTv <155215757+KurabeTV@users.noreply.github.com>
2025-10-14 07:27:51 -0400

    dirty-008

--- src/vrt_mem.c
+++ src/vrt_mem.c
@@ -32,14 +32,35 @@ VRTresult VRTmem_Alloc(
     // IMPLEMENTATION
     size_t offsetAddr = 0; 
     VRTresult res = VRToffset_Alloc(&mem->offset, _size, &offsetAddr);
-    if(res == VRT_RESULT_MEM_NO_SPACE) {
+    
+    if(res == VRT_RESULT_NO_SPACE_IARRAY) {
+        // need to size-up mem->offset.pool
+    } else if(res == VRT_RESULT_NO_SPACE_IALLOC) {
+        // need to reserve more space
+        // If no space in pMem. 
+        if(mem->count >= mem->length) {
 
+        }
+
+        // Reserve new space.
+        void *nPage = NULL;
+#ifdef VRT_OS_WIN
+        nPage = VirtualAlloc(NULL, mVRTmem_MAP_BYTE, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+#endif
+        if(nPage == NULL)
+            return VRT_RESULT_NO_SPACE_VIRMEM;
+
+        VRToffset_Grow(&mem->offset, mVRTmem_MAP_SIZE);
+        mem->pMem[mem->count++] = nPage;
+
+        // Retry alloc;
         res = VRToffset_Alloc(&mem->offset, _size, &offsetAddr);
     }
     if(res != VRT_RESULT_SUCCESS)
         return res;
 
-    
+    *addr = offsetAddr % mVRTmem_MAP_BYTE + (size_t)mem->pMem[offsetAddr / mVRTmem_MAP_BYTE];
+    return VRT_RESULT_SUCCESS;
 }
 VRTresult VRTmem_Free(
     VRTmem *mem,
